464a63e99c333a9fb4bb741d5d8002f31db2c191
its-miller-time <ian_c_miller@icloud.com>
2020-01-11 18:19:34 -0500

    connected funcion to MongoDB

--- handler.js
+++ handler.js
@@ -1,18 +1,56 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Welcome to Tvlr boys',
-        input: event.message,
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
+dotenv.config()
+const connectorMongodb = mongoose.connect(process.env.uri);
+// const userSchema = require('../api/schemas/userSchema.js')
+
+const userSchema = new mongoose.Schema({
+  name: {
+      type: String,
+      required: true
+  },
+  email: {
+      type: String,
+      required: true
+  }, 
+  password: {
+      type: String,
+      required: true
+  }
+})
+
+const User = mongoose.model('User',userSchema, 'User');
+
+module.exports.hello = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+   connectorMongodb
+    .then(()=>{
+    User.findOne()
+    .then(user => callback(null,{
+      statusCode:200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
       },
-      null,
-      2
-    ),
+      body: JSON.stringify(user)
+    }))
+    .catch(err => console.log(err))
+    })
   };
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(
+  //     {
+  //       message: 'Welcome to Tvlr boys',
+  //       input: event.message,
+  //     },
+  //     null,
+  //     2
+  //   ),
+  // };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  // };
